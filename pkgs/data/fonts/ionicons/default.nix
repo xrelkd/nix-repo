76438c4bb3d001b37b080ef36ead47a92d7545ee
xrelkd <46590321+xrelkd@users.noreply.github.com>
2022-05-26 22:48:50 +0800

    fix(ionicons): fix `ionicons`

--- pkgs/data/fonts/ionicons/default.nix
+++ pkgs/data/fonts/ionicons/default.nix
@@ -5,20 +5,21 @@
 let
   pname = "ionicons";
   version = "6.0.1";
-
 in
 fetchFromGitHub rec {
   owner = "ionic-team";
   repo = pname;
   rev = "v${version}";
 
-  postFetch = ''
-    tar -xf $downloadedFile
+  sha256 = "sha256-UooZ+rnq7mxCzYQby2y7cOe/Clk8ir5ETOH3N2cPg8s=";
 
-    install -Dm644 ${pname}-${version}/docs/fonts/ionicons.ttf -t $out/share/fonts/truetype/
+  downloadToTemp = true;
+
+  postFetch = ''
+    cd $out
+    install -Dm644 docs/fonts/ionicons.ttf -t $out/share/fonts/truetype/
   '';
 
-  sha256 = "sha256-DoFsglxY/+CeBSxw6b1FRuMMrdLco3F1XCyUTmk0RBc=";
 
   meta = with lib; {
     homepage = "https://ionicons.com";
