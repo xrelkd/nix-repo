ae1d580a2635650aa38fa40aa4c5a1d399758d72
xrelkd <46590321+xrelkd@users.noreply.github.com>
2025-04-27 21:58:23 +0800

    feat: drop `eriksync`

--- default.nix
+++ default.nix
@@ -32,7 +32,6 @@ with pkgs;
   caracal = callPackage ./pkgs/tools/networking/caracal { };
   clipcat = callPackage ./pkgs/applications/misc/clipcat { };
   catix = callPackage ./pkgs/tools/networking/catix { };
-  eriksync = callPackage ./pkgs/tools/misc/eriksync { };
   gdv-dl = callPackage ./pkgs/tools/misc/gdv-dl { };
   norden = callPackage ./pkgs/applications/networking/cluster/norden { };
   tunelo = callPackage ./pkgs/tools/networking/tunelo { };
