7b38c601b62897ceb64b22b9c45d086d18f46037
xrelkd <46590321+xrelkd@users.noreply.github.com>
2026-08-05 16:47:23 +0800

    feat(firefox-hidpi): add desktop entry support
    - Copy the upstream Firefox desktop entry to the package output.
    - Modify the copied desktop entry to use the `firefox-hidpi` wrapper.
    - Ensure the `firefox-hidpi` package provides a functional desktop launcher.
    - Update the package version date.

--- pkgs/applications/networking/browsers/firefox-hidpi/default.nix
+++ pkgs/applications/networking/browsers/firefox-hidpi/default.nix
@@ -6,7 +6,7 @@
 
 stdenvNoCC.mkDerivation {
   pname = "firefox-hidpi";
-  version = "2019-07-14";
+  version = "2026-08-05";
 
   phases = [ "installPhase" ];
 
@@ -16,7 +16,13 @@ stdenvNoCC.mkDerivation {
 
   installPhase = ''
     mkdir -p $out/bin
+    mkdir -p $out/share/applications
 
     makeWrapper ${firefox}/bin/firefox $out/bin/firefox --set GDK_DPI_SCALE 1.04
+
+    cp ${firefox}/share/applications/firefox.desktop $out/share/applications/firefox.desktop
+
+    substituteInPlace $out/share/applications/firefox.desktop \
+      --replace "Exec=firefox" "Exec=$out/bin/firefox"
   '';
 }
